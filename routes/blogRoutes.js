b29d5c7d9b9cfae95c94e3c489fc2796080c7dbd
ishani tech <nabinjoshi400@gmail.com>
2024-01-22 02:24:38 +0545

    rich text editor test route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,16 +3,16 @@ const blogController = require("../controller/blogController");
 const auth = require("../middlewares/auth");
 
 // create
-router.post("", auth, blogController.create);
+router.post("", blogController.create);
 
 // get all
-router.get("/all", auth, blogController.getAll);
+router.get("/all", blogController.getAll);
 
 // get blog by id
-router.get("/:id", auth, blogController.getById);
+router.get("/:id", blogController.getById);
 
 // update
-router.put("/update/:id", auth, blogController.update);
+router.put("/update/:id", blogController.update);
 
 // delete
 router.delete("/:id", auth, blogController.delete);
