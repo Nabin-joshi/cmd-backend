73fb2e18e46bc7f9546d25c1fe8c42e0876cae9c
SushilKushwaha <sushilsinha733@gmail.com>
2024-06-29 16:48:35 +0545

    Provience Wise entry for Districts,RMs,PNGOs,Schools, In total sum of respective section should be displayed, when provence is selected provience wise data should be shown done

--- models/CMSModels/geoMap.js
+++ models/CMSModels/geoMap.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Province = new mongoose.Schema({
   provinceName: String,
   office: String,
+  districts: String,
+  RMs: String,
+  PNGOs: String,
+  schools: String,
 });
 
 const geoMapSchema = new mongoose.Schema({
